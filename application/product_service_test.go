70f7bc1e78c2ec85bb67db1e1c2a6f76845aee88
Jefferson Martins <jeffersonms@outlook.com>
2023-12-26 20:40:54 -0300

    merge

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -2,22 +2,23 @@ package application_test
 
 import (
 	"testing"
-	"github.com/jeffersonmartins/go-hexagonal/mocks/application"
+
 	gomock "github.com/golang/mock/gomock"
-	testify "github.com/stretchr/testify/require"
+	"github.com/jeffersonmartins/go-hexagonal/application"
+	mock_application "github.com/jeffersonmartins/go-hexagonal/application/mocks"
+	"github.com/stretchr/testify/require"
 )
 
 func TestProductService_Get(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	product := NewMockProductInterface(ctrl)
-	persistence := NewMockProductPersistenceInterface(ctrl)
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
 
 	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
 
-	service = &ProductService{persistence: persistence
-	}
+	service := application.ProductService{Persistence: persistence}
 
 	result, err := service.Get("abc")
 	require.Nil(t, err)
